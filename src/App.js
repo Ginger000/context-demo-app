4e81a102b108b635e071abf7a1ac00c49cf0c45c
Ginger000 <linj2@illinois.edu>
2021-10-14 16:12:53 -0500

    add Context API with class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider } from "./Contexts/ThemeContext";
+import { LanguageProvider } from "./Contexts/LanguageContext";
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Form />
-      </>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider>
+                <PageContent>
+                    <LanguageProvider>
+                        <Navbar />
+                        <Form />
+                    </LanguageProvider>
+                </PageContent>
+            </ThemeProvider>
+        );
+    }
 }
 
 export default App;
